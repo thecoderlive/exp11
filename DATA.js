ad583be14b5cb9239c1b035e424c59ea8dab8565
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 01:27:52 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "3e265376-a808-4561-a20e-479909aa0257",
+    "home_feed": [
+        {
+            "id": "4de65ec1-55c5-4245-bd27-e771948863dc",
+            "artist_name": "talk",
+            "home_carousel": [
+                {
+                    "id": "2ba3837a-b6c3-48be-8ac9-61e0b6638754",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+                },
+                {
+                    "id": "684bd800-f11b-4ad1-8073-a4bf134ee4c8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "2447e733-3480-47a6-9264-539ad3e2f0ef",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "49d1bf6d-87d7-4ca5-804a-a16af36da3d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png"
+                },
+                {
+                    "id": "16f0a5af-2adc-4791-96b5-955a19171dfb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                }
+            ],
+            "art_caption": "like",
+            "art_description": "Try talk federal open. Behind common unit collection everyone identify federal with. So artist somebody affect that only safe."
+        },
+        {
+            "id": "76e16789-f58a-4afc-ba91-9414fa62dd0c",
+            "artist_name": "see",
+            "home_carousel": [
+                {
+                    "id": "e740c71e-74b2-4bab-b38f-79c78cb348a3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png"
+                },
+                {
+                    "id": "0edb5ccc-21ba-461a-a49e-73a5d1700937",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "aa042255-c258-4afa-819f-89488c1e7c92",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "d5a63090-a312-400d-8feb-913ff29554f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/251.png"
+                },
+                {
+                    "id": "0fcc259c-edcb-4444-9edb-5ec4090594f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/250.png"
+                }
+            ],
+            "art_caption": "foreign",
+            "art_description": "Career ground once alone. Summer fall speech difference money six. News mission at visit six."
+        },
+        {
+            "id": "19a8d9a9-c6f4-4e66-807d-70a56c6080ba",
+            "artist_name": "when",
+            "home_carousel": [
+                {
+                    "id": "9ea2e1f3-2a86-4a9e-a85e-6cd606501b9b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png"
+                },
+                {
+                    "id": "b61aefe8-4902-4071-8f6b-1051ca3f6df2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                },
+                {
+                    "id": "fa40b8e3-c34c-42c6-9d16-adfa46eaf874",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                },
+                {
+                    "id": "7b219105-ccc8-4af9-9303-210df9e4ddb3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "7311a931-a59a-437a-857e-4c25e1c91d1e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png"
+                }
+            ],
+            "art_caption": "recently",
+            "art_description": "Space tough good pick relate eight. Message set model meet rest. Future clearly decade cut mind soldier method get."
+        },
+        {
+            "id": "28cf3ba3-3f0d-4a64-9e9c-bf0b05545ea0",
+            "artist_name": "character",
+            "home_carousel": [
+                {
+                    "id": "22f026de-7c2f-4ac7-ba0c-d7c03afc85c1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/314.png"
+                },
+                {
+                    "id": "d42a5435-11c2-47b1-b7ef-f1cb803e6df7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png"
+                },
+                {
+                    "id": "55607185-7a37-46d5-bd0b-2841e196172d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "a88d1ade-6b77-4167-93d4-1c464429c2ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/339.png"
+                },
+                {
+                    "id": "4dfa1e7c-cce7-4b37-a095-c10da3dd7c09",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                }
+            ],
+            "art_caption": "increase",
+            "art_description": "He subject bad send when entire. Training include reach benefit gun air compare matter."
+        },
+        {
+            "id": "7c2f0b3f-1616-40a0-a974-6990f492f1d3",
+            "artist_name": "cultural",
+            "home_carousel": [
+                {
+                    "id": "7a978056-de0a-44cc-89bb-09b29e7279dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "f27cdb6e-80be-4bf7-ace1-692cab057616",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/207.png"
+                },
+                {
+                    "id": "9c93671a-eb1c-445d-8be7-725b33ca5a3e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "5b2ddb58-131a-43c4-8f32-267804afbe77",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+                },
+                {
+                    "id": "57a6305e-3a7b-4a86-bd3a-dadbaf6664fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                }
+            ],
+            "art_caption": "almost",
+            "art_description": "Huge tell nor growth provide result. Attorney four least decade who relationship court test."
+        }
+    ]
+}
